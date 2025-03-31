1c8becf09b1f54f1729b7600c22cd5afaeb98555
lxxcp <lxxcp@163.com>
2025-04-01 07:19:28 +0800

    Update build.gradle.kts

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -127,7 +127,8 @@ dependencies {
         implementation(libs.androidx.media3.exoplayer.dash)
         implementation(libs.androidx.media3.ui)
     }
-
+    implementation(libs.androidx.media3.datasource.rtmp)
+    implementation(libs.androidx.media3.exoplayer.smoothstreaming)
     implementation("com.github.CarGuo.GSYVideoPlayer:gsyvideoplayer-java:v10.0.0")
     // implementation("com.github.CarGuo.GSYVideoPlayer:gsyvideoplayer-ex_so:v10.0.0")
 
