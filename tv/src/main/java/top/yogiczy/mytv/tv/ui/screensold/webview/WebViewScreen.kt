52189a28c2a2299f2e786c083e1b1ebb81eb7d26
lxxcp <lxxcp@163.com>
2025-05-06 21:13:28 +0800

    Add files via upload

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/webview/WebViewScreen.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/webview/WebViewScreen.kt
@@ -3,16 +3,18 @@ package top.yogiczy.mytv.tv.ui.screensold.webview
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
+import android.util.Log
 import android.view.MotionEvent
 import android.view.ViewGroup
 import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,290 +23,166 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import com.tencent.smtt.sdk.QbSdk
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import top.yogiczy.mytv.core.data.utils.Globals
-import top.yogiczy.mytv.core.data.utils.Logger
+import top.yogiczy.mytv.core.data.entities.channel.ChannelLine
 import top.yogiczy.mytv.tv.ui.material.Visibility
 import top.yogiczy.mytv.tv.ui.screensold.webview.components.WebViewPlaceholder
-import java.io.File
-import java.nio.charset.Charset
+//import top.yogiczy.mytv.core.data.utils.Logger
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebViewScreen(
     modifier: Modifier = Modifier,
-    urlProvider: () -> String = { "webview://https://tv.cctv.com/live/index.shtml" },
+    urlProvider: () -> String = { "https://tv.cctv.com/live/index.shtml" },
     onVideoResolutionChanged: (width: Int, height: Int) -> Unit = { _, _ -> },
 ) {
-    val url = urlProvider().removePrefix("webview://")
+    val url = urlProvider()
     var placeholderVisible by remember { mutableStateOf(true) }
     var placeholderMessage by remember { mutableStateOf("加载中...") }
-    val context = LocalContext.current
-    var isCoreReplaced by remember { mutableStateOf(false) }
+    val logger = remember { Logger.create("WebViewScreen") }
+
+    // 处理webview://前缀
+    val actualUrl = remember(url) {
+        val processedUrl = if (url.startsWith("webview://")) {
+            //logger.i("检测到webview://前缀，正在处理WebView URL")
+            url.substring("webview://".length)
+        } else {
+            url
+        }
+        //logger.i("WebView加载URL: $processedUrl")
+        processedUrl
+    }
 
     val onUpdatePlaceholderVisible = { visible: Boolean, message: String ->
         placeholderVisible = visible
         placeholderMessage = message
     }
 
-    LaunchedEffect(Unit) {
-        isCoreReplaced = WebViewManager.isCoreReplaced()
+    Box(modifier = modifier.fillMaxSize()) {
+        AndroidView(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxHeight()
+                .background(Color.Black),
+            factory = {
+                MyWebView(it).apply {
+                    webViewClient = MyClient(
+                        onPageStarted = { 
+                            placeholderVisible = true
+                            //logger.i("WebView开始加载页面")
+                        },
+                        onPageFinished = { 
+                            //logger.i("WebView页面加载完成")
+                        },
+                    )
 
-        if (!isCoreReplaced) {
-            WebViewManager.replaceCore(context)
-            isCoreReplaced = WebViewManager.isCoreReplaced()
-        }
-    }
+                    setBackgroundColor(Color.Black.toArgb())
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                    )
 
-    Visibility({ isCoreReplaced }) {
-        Box(modifier = modifier.fillMaxSize()) {
-            if (WebViewManager.existTbsX5()) {
-                AndroidView(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .fillMaxHeight()
-                        .background(Color.Black),
-                    factory = {
-                        X5WebView(it).apply {
-                            webViewClient = X5WebviewClient(
-                                onPageStarted = { 
-                                    placeholderVisible = true
-                                    placeholderMessage = "加载中..."
-                                },
-                                onPageFinished = { placeholderVisible = false },
-                            )
+                    settings.javaScriptEnabled = true
+                    settings.useWideViewPort = true
+                    settings.loadWithOverviewMode = true
+                    settings.domStorageEnabled = true
+                    //settings.databaseEnabled = true
+                    settings.loadsImagesAutomatically = false
+                    settings.blockNetworkImage = true
+                    settings.userAgentString =
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
+                    settings.cacheMode = WebSettings.LOAD_DEFAULT
+                    settings.javaScriptCanOpenWindowsAutomatically = true
+                    settings.setSupportZoom(false)
+                    settings.displayZoomControls = false
+                    settings.builtInZoomControls = false
+                    settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+                    settings.mediaPlaybackRequiresUserGesture = false
 
-                            setupCommonWebViewSettings()
-                            addJavascriptInterface(
-                                WebViewInterface(
-                                    onVideoResolutionChanged = onVideoResolutionChanged,
-                                    onUpdatePlaceholderVisible = onUpdatePlaceholderVisible
-                                ), "Android"
-                            )
-                        }
-                    },
-                    update = { it.loadUrl(url) },
-                )
-            } else {
-                AndroidView(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .fillMaxHeight()
-                        .background(Color.Black),
-                    factory = {
-                        SystemWebView(it).apply {
-                            webViewClient = SystemWebviewClient(
-                                onPageStarted = { 
-                                    placeholderVisible = true
-                                    placeholderMessage = "加载中..."
-                                },
-                                onPageFinished = { placeholderVisible = false },
-                            )
+                    isHorizontalScrollBarEnabled = false
+                    isVerticalScrollBarEnabled = false
+                    /*isClickable = false
+                    isFocusable = false
+                    isFocusableInTouchMode = false*/
 
-                            setupCommonWebViewSettings()
-                            addJavascriptInterface(
-                                WebViewInterface(
-                                    onVideoResolutionChanged = onVideoResolutionChanged,
-                                    onUpdatePlaceholderVisible = onUpdatePlaceholderVisible
-                                ), "Android"
-                            )
-                        }
-                    },
-                    update = { it.loadUrl(url) },
-                )
-            }
+                    addJavascriptInterface(
+                        MyWebViewInterface(
+                            onVideoResolutionChanged = onVideoResolutionChanged,
+                            onUpdatePlaceholderVisible = onUpdatePlaceholderVisible,
+                        ), "Android"
+                    )
+                }
+            },
+            update = { it.loadUrl(actualUrl) },
+        )
 
-            Visibility({ placeholderVisible }) {
-                WebViewPlaceholder(message = placeholderMessage)
-            }
+        Visibility({ placeholderVisible }) {
+            WebViewPlaceholder(message = placeholderMessage)
         }
     }
 }
 
-private fun Any.setupCommonWebViewSettings() {
-    when (this) {
-        is android.webkit.WebView -> {
-            setBackgroundColor(Color.Black.toArgb())
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-
-            settings.apply {
-                javaScriptEnabled = true
-                useWideViewPort = true
-                loadWithOverviewMode = true
-                domStorageEnabled = true
-                cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-                loadsImagesAutomatically = false
-                blockNetworkImage = true
-                userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
-                javaScriptCanOpenWindowsAutomatically = true
-                setSupportZoom(false)
-                displayZoomControls = false
-                builtInZoomControls = false
-                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-                mediaPlaybackRequiresUserGesture = false
-            }
-
-            isHorizontalScrollBarEnabled = false
-            isVerticalScrollBarEnabled = false
-            isClickable = false
-            isFocusable = false
-            isFocusableInTouchMode = false
-        }
-        is com.tencent.smtt.sdk.WebView -> {
-            setBackgroundColor(Color.Black.toArgb())
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-            )
-
-            settings.apply {
-                javaScriptEnabled = true
-                useWideViewPort = true
-                loadWithOverviewMode = true
-                domStorageEnabled = true
-                cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-                loadsImagesAutomatically = false
-                blockNetworkImage = true
-                userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
-                javaScriptCanOpenWindowsAutomatically = true
-                setSupportZoom(false)
-                displayZoomControls = false
-                builtInZoomControls = false
-                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-                mediaPlaybackRequiresUserGesture = false
-            }
-
-            isHorizontalScrollBarEnabled = false
-            isVerticalScrollBarEnabled = false
-            isClickable = false
-            isFocusable = false
-            isFocusableInTouchMode = false
-        }
-    }
-}
-
-private class SystemWebviewClient(
+class MyClient(
     private val onPageStarted: () -> Unit,
     private val onPageFinished: () -> Unit,
-) : android.webkit.WebViewClient() {
+) : WebViewClient() {
+    private val logger = Logger.create("WebViewClient")
+    
+    // override fun shouldInterceptRequest(
+    //     view: WebView?,
+    //     request: WebResourceRequest?
+    // ): WebResourceResponse? {
+    //     if (request?.url.toString().endsWith(".css"))
+    //         return WebResourceResponse("text/css", "UTF-8", null)
+    //     return null
+    // }
 
-    override fun onPageStarted(view: android.webkit.WebView?, url: String?, favicon: Bitmap?) {
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        //logger.i("WebView页面开始加载: $url")
         onPageStarted()
         super.onPageStarted(view, url, favicon)
     }
 
-    private fun readAssetFile(context: Context, fileName: String): String {
-        return context.assets.open(fileName).use { inputStream ->
-            val size = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer)
-            String(buffer, Charset.forName("UTF-8"))
-        }
+    fun readAssetFile(context: Context, fileName: String): String {
+        val inputStream = context.assets.open(fileName)
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        return String(buffer, Charsets.UTF_8)
     }
 
-    override fun onPageFinished(view: android.webkit.WebView, url: String) {
+    override fun onPageFinished(view: WebView, url: String) {
+        //logger.i("WebView页面加载完成: $url")
         val scriptContent = readAssetFile(view.context, "webview_player.js")
-        view.evaluateJavascript(scriptContent.trimIndent()) {
+        //logger.i("注入脚本到WebView")
+        view.evaluateJavascript(scriptContent.trimIndent()
+        ) {
+            //logger.i("脚本注入完成")
             onPageFinished()
         }
     }
 }
 
-private class SystemWebView(context: Context) : android.webkit.WebView(context) {
+class MyWebView(context: Context) : WebView(context) {
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         return false
     }
 }
 
-private class X5WebviewClient(
-    private val onPageStarted: () -> Unit,
-    private val onPageFinished: () -> Unit,
-) : com.tencent.smtt.sdk.WebViewClient() {
-
-    override fun onPageStarted(
-        view: com.tencent.smtt.sdk.WebView?,
-        url: String?,
-        favicon: Bitmap?
-    ) {
-        onPageStarted()
-        super.onPageStarted(view, url, favicon)
-    }
-
-    private fun readAssetFile(context: Context, fileName: String): String {
-        return context.assets.open(fileName).use { inputStream ->
-            val size = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer)
-            String(buffer, Charset.forName("UTF-8"))
-        }
-    }
-
-    override fun onPageFinished(view: com.tencent.smtt.sdk.WebView, url: String) {
-        val scriptContent = readAssetFile(view.context, "webview_player.js")
-        view.evaluateJavascript(scriptContent.trimIndent()) {
-            onPageFinished()
-        }
-    }
-}
-
-private class X5WebView(context: Context) : com.tencent.smtt.sdk.WebView(context) {
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return false
-    }
-}
-
-private class WebViewInterface(
+class MyWebViewInterface(
     private val onVideoResolutionChanged: (width: Int, height: Int) -> Unit = { _, _ -> },
-    private val onUpdatePlaceholderVisible: (visible: Boolean, message: String) -> Unit = { _, _ -> }
+    private val onUpdatePlaceholderVisible: (visible: Boolean, message: String) -> Unit,
 ) {
     @JavascriptInterface
     fun changeVideoResolution(width: Int, height: Int) {
         onVideoResolutionChanged(width, height)
         onUpdatePlaceholderVisible(false, "")
     }
-}
 
-object WebViewManager {
-    private val log = Logger.create("WebViewManager")
-    private var coreHasReplaced = false
-
-    fun existTbsX5(): Boolean {
-        val tbsX5File = File(Globals.nativeLibraryDir, "libtbs.libmttwebview.so.so")
-        return tbsX5File.exists()
-    }
-
-    fun isCoreReplaced() = coreHasReplaced
-
-    suspend fun replaceCore(context: Context) = withContext(Dispatchers.IO) {
-        log.d("Starting core replacement process")
-
-        if (coreHasReplaced) {
-            log.d("Core has already been replaced, skipping replacement")
-            return@withContext
-        }
-        coreHasReplaced = true
-
-        if (!existTbsX5()) {
-            log.e("TBS core file not found. Aborting core replacement.")
-            return@withContext
-        }
-
-        if (QbSdk.canLoadX5(context)) {
-            log.d("X5 core is already loaded. No replacement needed.")
-            return@withContext
-        }
-
-        log.w("X5 core is not loaded. Attempting to preinstall TBS.")
-        val ok = QbSdk.preinstallStaticTbs(context)
-        log.d("TBS core preinstallation initiated: $ok")
+    @JavascriptInterface
+    fun updatePlaceholderVisible(visible: Boolean, message: String) {
+        onUpdatePlaceholderVisible(visible, message)
     }
 }
