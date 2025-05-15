7f32d91053f293ad01733cf876000d08b79a1641
lxxcp <lxxcp@163.com>
2025-05-15 12:39:25 +0800

    Update WebViewScreen.kt

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/webview/WebViewScreen.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screensold/webview/WebViewScreen.kt
@@ -96,7 +96,7 @@ fun WebViewScreen(
                     settings.domStorageEnabled = true
                     settings.loadsImagesAutomatically = false
                     settings.blockNetworkImage = true
-                    settings.userAgentString =                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
+                    settings.userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
                     settings.cacheMode = WebSettings.LOAD_DEFAULT
                     settings.javaScriptCanOpenWindowsAutomatically = true
                     settings.setSupportZoom(false)
